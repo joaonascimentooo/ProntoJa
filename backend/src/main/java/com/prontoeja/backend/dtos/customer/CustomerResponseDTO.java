ca872efc075d69681c5c29efcecb4a64f25d1d3d
joaonascimentooo <joaonascimento197@gmail.com>
2025-11-30 14:16:02 -0300

    feat: add CustomerRequestDTO and CustomerResponseDTO classes; remove unused CustomerRepository reference in CustomerService

--- backend/src/main/java/com/prontoeja/backend/dtos/customer/CustomerResponseDTO.java
+++ backend/src/main/java/com/prontoeja/backend/dtos/customer/CustomerResponseDTO.java
@@ -1,13 +1,13 @@
-package com.prontoeja.backend.dtos;
+package com.prontoeja.backend.dtos.customer;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class CustomerRequestDTO {
+public class CustomerResponseDTO {
+    private String id;
     private String name;
     private String email;
     private String phoneNumber;
-    private String password;
 }
--- backend/src/main/java/com/prontoeja/backend/services/CustomerService.java
+++ backend/src/main/java/com/prontoeja/backend/services/CustomerService.java
@@ -2,14 +2,10 @@ package com.prontoeja.backend.services;
 
 import org.springframework.stereotype.Service;
 
-import com.prontoeja.backend.repositories.CustomerRepository;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
-
-    private final CustomerRepository customerRepository;
 
 }
